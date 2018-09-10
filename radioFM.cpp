68251a8a4b3e15575c0a9715c8a40badbcd8cd46
zagii <zagawus@interia.pl>
2018-09-10 17:24:07 +0200

    opakowanie w klase

--- radioFM.cpp
+++ radioFM.cpp
@@ -1,151 +1,34 @@
-///
-/// \file SerialRadio.ino 
-/// \brief Radio implementation using the Serial communication.
-/// 
-/// \author Matthias Hertel, http://www.mathertel.de
-/// \copyright Copyright (c) 2014 by Matthias Hertel.\n
-/// This work is licensed under a BSD style license.\n
-/// See http://www.mathertel.de/License.aspx
-///
-/// \details
-/// This is a full function radio implementation that uses a LCD display to show the current station information.\n
-/// It can be used with various chips after adjusting the radio object definition.\n
-/// Open the Serial console with 57600 baud to see current radio information and change various settings.
-///
-/// Wiring
-/// ------
-/// The necessary wiring of the various chips are described in the Testxxx example sketches.
-/// The boards have to be connected by using the following connections:
-/// 
-/// Arduino port | SI4703 signal | RDA5807M signal
-/// :----------: | :-----------: | :-------------:
-///          GND | GND           | GND   
-///         3.3V | VCC           | -
-///           5V | -             | VCC
-///           A5 | SCLK          | SCLK
-///           A4 | SDIO          | SDIO
-///           D2 | RST           | -
-///
-///
-/// More documentation and source code is available at http://www.mathertel.de/Arduino
-///
-/// History:
-/// --------
-/// * 05.08.2014 created.
-/// * 04.10.2014 working.
-
-#include <Wire.h>
-
-#include <radio.h>
-#include <RDA5807M.h>
-#include <SI4703.h>
-#include <SI4705.h>
-#include <TEA5767.h>
-
-#include <RDSParser.h>
+#include "radioFM.h"
 
 
-// Define some stations available at your locations here:
-// 89.40 MHz as 8940
-/*
-RADIO_FREQ preset[] = {
-  8770,
-  8810, // hr1
-  8820,
-  8850, // Bayern2
-  8890, // ???
-  8930, // * hr3
-  8980,
-  9180,
-  9220, 9350,
-  9440, // * hr1
-  9510, // - Antenne Frankfurt
-  9530,
-  9560, // Bayern 1
-  9680, 9880,
-  10020, // planet
-  10090, // ffh
-  10110, // SWR3
-  10030, 10260, 10380, 10400,
-  10500 // * FFH
-};*/
-RADIO_FREQ preset[] = {
-  8840,//   Radio Pogoda (Warszawa)   Rondo 1
-  8900 ,//  Radio Maryja Radio Watykañskie   Pa³ac Kultury i Nauki
-  8940 ,//  Radio Hobby   Legionowo, Komin PEC
-  8980 ,//  Radio WAWA  Rondo 1
-  9060 ,//  RMF FM  Rondo 1
-  9100 ,//  RMF FM  RTCN Raszyn
-  9200 ,//  Polskie Radio 24  Hotel Marriott
-  9240 ,//  Polskie Radio Jedynka   Hotel Marriott
-  9280 ,//  POP Radio   Pruszków, Komin EC Pruszków II
-  9330 ,//  Eska ROCK   Rondo 1
-  9400 ,//  Meloradio   Pa³ac Kultury i Nauki
-  9450 ,//  Radio Bogoria   Grodzisk Mazowiecki, Komin Gedeon Richter
-  9470 ,//  Radio FaMa (Wo³omin)  Wo³omin, Komin ZEC
-  9510  ,// Katolickie Radio Zbrosza Du¿a Radio Maryja  Zbrosza Du¿a
-  9540 ,//  Radio Maryja Radio Watykañskie   Skierniewice, RON Bartniki
-  9580,//   RMF Maxxx   Pa³ac Kultury i Nauki
-  9650 ,//  Radio Plus (Warszawa)   Pa³ac Kultury i Nauki
-  9710 ,//  Radio Kampus  Rondo 1
-  9770 ,//  Radio Tok FM  Pa³ac Kultury i Nauki
-  9810 ,//  Radio Victoria  Mszczonów, Wie¿a Ciœnieñ
-  9830 ,//  RMF Classic   Pa³ac Kultury i Nauki
-  9880 ,//  Polskie Radio Trójka  RTCN Raszyn
-  9910 ,//  Polskie Radio Trójka  Hotel Marriott
-  9950 ,//  Radio Pogoda (Warszawa)   Rondo 1 (0-12 godz)
-  9950 ,//  Radio Armii Krajowej Jutrzenka  ul. Dereniowa (12-24 godz)
-10010 ,// Radio Z³ote Przeboje (Warszawa)   Hotel Marriott
-10100 ,// Polskie Radio RDC   Pa³ac Kultury i Nauki
-10150 ,// Chillizet   Pa³ac Kultury i Nauki
-10170 ,// Katolickie Radio Podlasie   Siedlce, RTCN £osice
-10200 ,// Radio Muzo.FM   Pa³ac Kultury i Nauki
-10240 ,// Polskie Radio Jedynka   RTCN Raszyn
-10270 ,// Radio Niepokalanów  Skierniewice, RON Bartniki
-10300 ,// Radio Kolor   Pa³ac Kultury i Nauki
-10340 ,// Polskie Radio RDC ?????????   Siedlce, RTCN £osice
-10370 ,// Rock Radio (Warszawa)   Pa³ac Kultury i Nauki
-10410 ,// Radio FaMa (¯yrardów)   ¯yrardów, Komin PEC ¯yrardów
-10440 ,// Radio Vox FM  Pa³ac Kultury i Nauki
-10490 ,// Polskie Radio Dwójka  Hotel Marriott
-10560 ,// Radio Eska (Warszawa)   Rondo 1
-10620 ,// Radio Warszawa Radio Watykañskie   Pa³ac Kultury i Nauki
-10640 ,// Radio Vox FM  Grójec, ul. Mogielnicka
-10680 ,// Antyradio   Pa³ac Kultury i Nauki
-10750 // Radio ZET   Pa³ac Kultury i Nauki
-};
-int    i_sidx=20;        ///< Start at Station with index=5
 
-/// The radio object has to be defined by using the class corresponding to the used chip.
-/// by uncommenting the right radio object definition.
+/// Update the ServiceName text on the LCD display.
+void DisplayServiceName(char *name)
+{
+  Serial.print("RDS:"); 
+  rdsLastName=name;
+   Serial.println(name);
+} // DisplayServiceName()
 
-// RADIO radio;       ///< Create an instance of a non functional radio.
-// RDA5807M radio;    ///< Create an instance of a RDA5807 chip radio
- SI4703   radio;    ///< Create an instance of a SI4703 chip radio.
-//SI4705   radio;    ///< Create an instance of a SI4705 chip radio.
-// TEA5767  radio;    ///< Create an instance of a TEA5767 chip radio.
-
-
-/// get a RDS parser
-RDSParser rds;
-
-
-/// State definition for this radio implementation.
-enum RADIO_STATE {
-  STATE_PARSECOMMAND, ///< waiting for a new command character.
-  
-  STATE_PARSEINT,     ///< waiting for digits for the parameter.
-  STATE_EXEC          ///< executing the command.
-};
-
-RADIO_STATE state; ///< The state variable is used for parsing input characters.
-
+void receiveTextRDS(char *name)
+{
+  Serial.print("RDS text:");
+  rdsLastText=name;
+  Serial.println(rdsLastText);
+}
+void receiveTimeRDS(uint8_t h,uint8_t m)
+{
+  Serial.print("RDS time:");
+  rdsLastTime=h+":"+m;
+  Serial.println(rdsLastTime);
+}
+void RDS_process(uint16_t block1, uint16_t block2, uint16_t block3, uint16_t block4) {
+  rds.processData(block1, block2, block3, block4);
+}
 // - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-
-
 /// Update the Frequency on the LCD display.
-void DisplayFrequency(RADIO_FREQ f)
+void CradioFM::DisplayFrequency(RADIO_FREQ f)
 {
   char s[12];
   radio.formatFrequency(s, sizeof(s));
@@ -153,27 +36,14 @@ void DisplayFrequency(RADIO_FREQ f)
 } // DisplayFrequency()
 
 
-/// Update the ServiceName text on the LCD display.
-void DisplayServiceName(char *name)
-{
-  Serial.print("RDS:");
-  Serial.println(name);
-} // DisplayServiceName()
 
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-
-void RDS_process(uint16_t block1, uint16_t block2, uint16_t block3, uint16_t block4) {
-  rds.processData(block1, block2, block3, block4);
-}
 
 
 /// Execute a command identified by a character and an optional number.
 /// See the "?" command for available commands.
 /// \param cmd The command character.
 /// \param value An optional parameter for the command.
-void runSerialCommand(char cmd, int16_t value)
+void CradioFM::runSerialCommand(char cmd, int16_t value)
 {
   if (cmd == '?') {
     Serial.println();
@@ -261,12 +131,7 @@ void runSerialCommand(char cmd, int16_t value)
 
 
 /// Setup a FM only radio configuration with I/O for commands and debugging on the Serial port.
-void setup() {
-  // open the Serial port
-  Serial.begin(115200);
-  Serial.print("Radio...");
-  delay(500);
-
+void CradioFM::begin() {
   // Initialize the Radio 
   radio.init();
 
@@ -277,7 +142,7 @@ void setup() {
 
   // delay(100);
 
-  radio.setMono(false);
+  radio.setMono(true);
   radio.setMute(false);
   // radio.debugRegisters();
   radio.setVolume(8);
@@ -289,24 +154,14 @@ void setup() {
   // setup the information chain for RDS data.
   radio.attachReceiveRDS(RDS_process);
   rds.attachServicenNameCallback(DisplayServiceName);
+  rds.attachTextCallback(receiveTextRDS);
+  rds.attachTimeCallback(receiveTimeRDS);
   
   runSerialCommand('?', 0);
 } // Setup
 
-
-/// Constantly check for serial input commands and trigger command execution.
-void loop() {
-  int newPos;
-  unsigned long now = millis();
-  static unsigned long nextFreqTime = 0;
-  static unsigned long nextRadioInfoTime = 0;
-  
-  // some internal static values for parsing the input
-  static char command;
-  static int16_t value;
-  static RADIO_FREQ lastf = 0;
-  RADIO_FREQ f = 0;
-  
+void CradioFM::checkSerial()
+{
   char c;
   if (Serial.available() > 0) {
     // read the next char from input.
@@ -339,10 +194,11 @@ void loop() {
     } // if
   } // if
 
+}
 
-  // check for RDS data
-  radio.checkRDS();
-
+CradioFM::printStatus()
+{
+   unsigned long now = millis();
   // update the display from time to time
   if (now > nextFreqTime) {
     f = radio.getFrequency();
@@ -354,6 +210,48 @@ void loop() {
     nextFreqTime = now + 400;
   } // if  
 
+}
+void CRadio::setStatusStr()
+{
+ int v = radio.getVolume();
+ RADIO_FREQ ff = radio.getFrequency();
+    statusStr="{'freq':"+ff+", 'vol':"+v+",'rdsName':'"+ rdsLastName+
+                "','rdsText':'"+rdsLastText+
+                "','rdsTime':'"+rdsLastTime+"'}";
+}
+void CradioFM::testRDS()
+{
+  bool ret=false;
+  radio.checkRDS(); // check for RDS data
+  if(byloSN)
+  {
+    ret=true;
+    byloSN=false;
+  }
+  if(byloText)
+  {
+    ret=true;
+    byloText=false;
+  }
+  if(byloTime)
+  {
+    ret=true;
+    byloTime=false;
+  }
+  if(ret)
+  {
+    setStatusStr();
+  }
+  return ret;
+}
+/// Constantly check for serial input commands and trigger command execution.
+bool CradioFM::loop() 
+{
+  checkSerial(); //sprawdza komende z Seriala
+  bool test=testRDS();
+   
+  printStatus();  
+  return test;
 } // loop
 
 // End.
---
+++ radioFM.h
@@ -0,0 +1,102 @@
+#ifndef CradioFM_H
+#define CradioFM_H
+
+
+#include <Wire.h>
+
+#include <radio.h>
+#include <SI4703.h>
+#include <RDSParser.h>
+
+RADIO_FREQ preset[] = {
+  8840,//   Radio Pogoda (Warszawa)   Rondo 1
+  8900 ,//  Radio Maryja Radio Watyka�skie   Pa�ac Kultury i Nauki
+  8940 ,//  Radio Hobby   Legionowo, Komin PEC
+  8980 ,//  Radio WAWA  Rondo 1
+  9060 ,//  RMF FM  Rondo 1
+  9100 ,//  RMF FM  RTCN Raszyn
+  9200 ,//  Polskie Radio 24  Hotel Marriott
+  9240 ,//  Polskie Radio Jedynka   Hotel Marriott
+  9280 ,//  POP Radio   Pruszk�w, Komin EC Pruszk�w II
+  9330 ,//  Eska ROCK   Rondo 1
+  9400 ,//  Meloradio   Pa�ac Kultury i Nauki
+  9450 ,//  Radio Bogoria   Grodzisk Mazowiecki, Komin Gedeon Richter
+  9470 ,//  Radio FaMa (Wo�omin)  Wo�omin, Komin ZEC
+  9510  ,// Katolickie Radio Zbrosza Du�a Radio Maryja  Zbrosza Du�a
+  9540 ,//  Radio Maryja Radio Watyka�skie   Skierniewice, RON Bartniki
+  9580,//   RMF Maxxx   Pa�ac Kultury i Nauki
+  9650 ,//  Radio Plus (Warszawa)   Pa�ac Kultury i Nauki
+  9710 ,//  Radio Kampus  Rondo 1
+  9770 ,//  Radio Tok FM  Pa�ac Kultury i Nauki
+  9810 ,//  Radio Victoria  Mszczon�w, Wie�a Ci�nie�
+  9830 ,//  RMF Classic   Pa�ac Kultury i Nauki
+  9880 ,//  Polskie Radio Tr�jka  RTCN Raszyn
+  9910 ,//  Polskie Radio Tr�jka  Hotel Marriott
+  9950 ,//  Radio Pogoda (Warszawa)   Rondo 1 (0-12 godz)
+  9950 ,//  Radio Armii Krajowej Jutrzenka  ul. Dereniowa (12-24 godz)
+10010 ,// Radio Z�ote Przeboje (Warszawa)   Hotel Marriott
+10100 ,// Polskie Radio RDC   Pa�ac Kultury i Nauki
+10150 ,// Chillizet   Pa�ac Kultury i Nauki
+10170 ,// Katolickie Radio Podlasie   Siedlce, RTCN �osice
+10200 ,// Radio Muzo.FM   Pa�ac Kultury i Nauki
+10240 ,// Polskie Radio Jedynka   RTCN Raszyn
+10270 ,// Radio Niepokalan�w  Skierniewice, RON Bartniki
+10300 ,// Radio Kolor   Pa�ac Kultury i Nauki
+10340 ,// Polskie Radio RDC ?????????   Siedlce, RTCN �osice
+10370 ,// Rock Radio (Warszawa)   Pa�ac Kultury i Nauki
+10410 ,// Radio FaMa (�yrard�w)   �yrard�w, Komin PEC �yrard�w
+10440 ,// Radio Vox FM  Pa�ac Kultury i Nauki
+10490 ,// Polskie Radio Dw�jka  Hotel Marriott
+10560 ,// Radio Eska (Warszawa)   Rondo 1
+10620 ,// Radio Warszawa Radio Watyka�skie   Pa�ac Kultury i Nauki
+10640 ,// Radio Vox FM  Gr�jec, ul. Mogielnicka
+10680 ,// Antyradio   Pa�ac Kultury i Nauki
+10750 // Radio ZET   Pa�ac Kultury i Nauki
+};
+/// State definition for this radio implementation.
+enum RADIO_STATE {
+  STATE_PARSECOMMAND, ///< waiting for a new command character.
+  
+  STATE_PARSEINT,     ///< waiting for digits for the parameter.
+  STATE_EXEC          ///< executing the command.
+};
+// callback functions for RDS
+RDSParser rds;/// get a RDS parser
+void DisplayServiceName(char *name);
+void receiveTextRDS(char *name);
+void receiveTimeRDS(uint8_t h,uint8_t m);
+void RDS_process(uint16_t block1, uint16_t block2, uint16_t block3, uint16_t block4);
+bool byloSN=false;
+bool byloText=false;
+bool byloTime=false;
+String rdsLastName="";
+String rdsLastText="";
+String rdsLastTime="";
+
+class CradioFM
+{
+    int    i_sidx=20;        ///< Start at Station with index=5
+    SI4703   radio;    ///< Create an instance of a SI4703 chip radio.
+
+    RADIO_STATE state; ///< The state variable is used for parsing input characters.
+    static unsigned long nextFreqTime = 0;
+    static unsigned long nextRadioInfoTime = 0;
+    RADIO_FREQ f = 0;
+    static RADIO_FREQ lastf = 0;
+       // some internal static values for parsing the input
+    static char command;
+    static int16_t value;
+     // int newPos;
+    String statusStr="";
+    bool testRDS();
+
+    public:
+    void DisplayFrequency(RADIO_FREQ f);
+    void runSerialCommand(char cmd, int16_t value);
+    void begin();
+    bool loop(); //zwraca true jesli zmienil sie status
+    void checkSerial();
+    void setStatusStr();
+    String getStatusStr(){return statusStr;}
+};
+#endif
